95a8dddfebd7179a26bd80cb3802652bf47a3a75
Alexey <idreamzzer@gmail.com>
2023-08-30 18:25:13 +0300

    feat: cleaned css, window alwaysOnTop, basic Button

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { appWindow } from "@tauri-apps/api/window";
+
+await appWindow.setAlwaysOnTop(true);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
