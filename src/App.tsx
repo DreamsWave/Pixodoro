be35cb23e3532adfdc8aac249ad11968615bf163
Alexey <idreamzzer@gmail.com>
2023-08-31 18:49:32 +0300

    feat: bresenham's drawing circle algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
+import PixelCircle from "./components/PixelCircle";
 
 function App() {
-  const [pixelSize, setPixelSize] = useState<number>(4);
+  const [pixelSize, setPixelSize] = useState<number>(8);
   return (
     <>
-      <Button pixelSize={pixelSize}>My button</Button>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <Button pixelSize={pixelSize}>My button</Button>
+        <PixelCircle pixelSize={pixelSize} />
+      </div>
     </>
   );
 }
