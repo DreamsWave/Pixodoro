a9555a296c8999d1a214f8523e44cb5f5f6de548
Alexey <dreamswave.dev@gmail.com>
2023-09-15 15:13:20 +0300

    feat: added redux-persist for saving state after restarting app

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,34 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  ThunkAction,
+  Action,
+  combineReducers,
+} from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import timerReducer from "./features/timer/timerSlice";
 import appSettingsReducer from "./features/appSettings/appSettingsSlice";
 import musicReducer from "./features/music/musicSlice";
 
-export const store = configureStore({
-  reducer: {
-    timer: timerReducer,
-    appSettings: appSettingsReducer,
-    music: musicReducer,
-  },
+const rootReducer = combineReducers({
+  timer: timerReducer,
+  appSettings: appSettingsReducer,
+  music: musicReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
