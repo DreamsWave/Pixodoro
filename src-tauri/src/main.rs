b378b9fb5d2c365af70661fb427cc756c6058ad7
Alexey <idreamzzer@gmail.com>
2023-09-26 19:36:38 +0300

    feat: added tray app like behavior and tray icon for timer progress stage

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,75 +1,84 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{
-    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
-};
+use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri_plugin_positioner::{Position, WindowExt};
+
+use std::env;
+
+// tauri command that updates the tray icon
+#[tauri::command]
+async fn update_tray_icon(app_handle: tauri::AppHandle, status: String, progress: i32) {
+    let mut image_path = format!(
+        "icons/tray/{status}/favicon-{status}-{progress}.png",
+        status = status,
+        progress = progress
+    );
+    if progress == 0 {
+        image_path = format!("icons/tray/favicon-default.png");
+    }
+    app_handle
+        .tray_handle()
+        .set_icon(tauri::Icon::File(
+            app_handle
+                .path_resolver()
+                .resolve_resource(image_path)
+                .unwrap(),
+        ))
+        .unwrap();
+    let _ = app_handle
+        .tray_handle()
+        .set_tooltip(&(progress.to_string() + "%").to_owned());
+}
 
 fn main() {
-    let hide = CustomMenuItem::new("hide".to_string(), "Minimize to Tray");
-    let quit = CustomMenuItem::new("quit".to_string(), "Exit");
-    let tray_menu = SystemTrayMenu::new()
-        .add_item(hide)
-        .add_native_item(SystemTrayMenuItem::Separator)
-        .add_item(quit);
-    let system_tray = SystemTray::new().with_menu(tray_menu);
+    env::set_var("RUST_BACKTRACE", "1");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let system_tray_menu = SystemTrayMenu::new().add_item(quit);
+    let system_tray = SystemTray::new().with_menu(system_tray_menu);
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_positioner::init())
+        .invoke_handler(tauri::generate_handler![update_tray_icon])
         .on_window_event(|event| match event.event() {
             tauri::WindowEvent::CloseRequested { api, .. } => {
                 event.window().hide().unwrap();
                 api.prevent_close();
             }
+            tauri::WindowEvent::Focused(is_focused) => {
+                if !is_focused {
+                    event.window().hide().unwrap();
+                }
+            }
             _ => {}
         })
         .system_tray(system_tray)
-        .on_system_tray_event(|app, event| match event {
-            SystemTrayEvent::LeftClick {
-                position: _,
-                size: _,
-                ..
-            } => {
-                println!("system tray received a left click");
-                let window = app.get_window("main").unwrap();
-                window.show().unwrap();
-            }
-
-            SystemTrayEvent::RightClick {
-                position: _,
-                size: _,
-                ..
-            } => {
-                println!("system tray received a right click");
-                let window = app.get_window("main").unwrap();
-                let item_handle = app.tray_handle().get_item("hide");
-                if window.is_visible().unwrap() {
-                    item_handle.set_title("Minimize to Tray").unwrap();
-                } else {
-                    item_handle.set_title("Show pixodoro").unwrap();
-                }
-            }
-
-            SystemTrayEvent::MenuItemClick { id, .. } => {
-                let item_handle = app.tray_handle().get_item(&id);
-                match id.as_str() {
-                    "hide" => {
-                        let window = app.get_window("main").unwrap();
-                        if window.is_visible().unwrap() {
-                            window.hide().unwrap();
-                            item_handle.set_title("Show pixodoro").unwrap();
-                        } else {
-                            window.show().unwrap();
-                            item_handle.set_title("Minimize to Tray").unwrap();
-                        }
+        .on_system_tray_event(|app, event| {
+            tauri_plugin_positioner::on_tray_event(app, &event);
+            match event {
+                SystemTrayEvent::LeftClick {
+                    position: _,
+                    size: _,
+                    ..
+                } => {
+                    let window = app.get_window("main").unwrap();
+                    // use TrayCenter as initial window position
+                    let _ = window.move_window(Position::TrayCenter);
+                    if window.is_visible().unwrap() {
+                        window.hide().unwrap();
+                    } else {
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
                     }
+                }
+                SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
                     "quit" => {
                         std::process::exit(0);
                     }
                     _ => {}
-                }
-                // app.tray_handle().set_icon(tauri::Icon::Raw(include_bytes!("../icons/tray/pomodoro/favicon-pomodoro-50.png").to_vec())).unwrap();
+                },
+                _ => {}
             }
-            _ => {}
         })
         .build(tauri::generate_context!())
         .expect("error while running tauri application")
