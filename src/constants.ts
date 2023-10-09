7db43869d2bd58bd9fb7775e8fb5f7bf1f23f0be
Alexey <dreamswave.dev@gmail.com>
2023-10-09 18:27:34 +0300

    feat: added notification switcher refactor: timer component logic

--- src/constants.ts
+++ src/constants.ts
@@ -262,3 +262,22 @@ export const autoModeIconPixelPositions = [
   [0, 4],
   [3, 4],
 ];
+
+export const notificationIconPixelPositions = [
+  [2, 0],
+  [1, 1],
+  [2, 1],
+  [3, 1],
+  [1, 2],
+  [2, 2],
+  [3, 2],
+  [1, 3],
+  [2, 3],
+  [3, 3],
+  [0, 4],
+  [1, 4],
+  [2, 4],
+  [3, 4],
+  [4, 4],
+  [2, 5],
+];
--- src/features/notification/notificationSlice.ts
+++ src/features/notification/notificationSlice.ts
@@ -1,26 +1,25 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { RootState } from "../../store";
-import { isPermissionGranted } from "@tauri-apps/api/notification";
 
 export interface NotificationState {
-  permissionGranted: boolean;
+  enabled: boolean;
 }
 
 const initialState: NotificationState = {
-  permissionGranted: (await isPermissionGranted()) ?? false,
+  enabled: false,
 };
 
 export const notificationSlice = createSlice({
   name: "notification",
   initialState,
   reducers: {
-    setPermissionGranted: (state, action: PayloadAction<boolean>) => {
-      state.permissionGranted = action.payload;
+    setEnabled: (state, action: PayloadAction<boolean>) => {
+      state.enabled = action.payload;
     },
   },
 });
 
-export const { setPermissionGranted } = notificationSlice.actions;
+export const { setEnabled } = notificationSlice.actions;
 
 export const selectNotification = (state: RootState) => state.notification;
 
